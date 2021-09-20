172119cb456bede02ba4c052b55345b479a8e51a
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-20 17:12:38 +0100

    Assignment Complete!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Comment2 from "./Comment"
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Comment2 />
     </div>
   );
 }
--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,8 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
-export default Comment;
+function Comment2(){
+  return <div>Dude seems smart for sure.</div>
+}
+
+export default Comment;Comment2;
